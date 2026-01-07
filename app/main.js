ba4d5f00fb21d4c22bf620abb80b1d2a71e8be8e
VlBl <v95506442@gmail.com>
2026-01-07 23:07:47 +0200

    finals

--- app/main.js
+++ app/main.js
@@ -3,4 +3,5 @@
 export function add(a, b) {
   return a + b;
 }
-console.log('3 + 2 =', add(3, 2));
+
+console.log("3 + 2 =", add(3, 2));
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,8 +1,8 @@
 import { test } from "node:test";
-import { strict as assert } from "node:assert";
+import { strict} from "node:assert";
 import { add } from "../app/main.js";
 
 test('3 + 2', () => {
   const result = add(3, 2);
-  assert.strictEqual(result, 5);
+  strict.strictEqual(result, 5);
 });
